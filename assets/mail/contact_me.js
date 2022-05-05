abc9f127d914df6b03f46d1926f22ada8ce430fb
DanielOpanubi <99402393+DanielOpanubi@users.noreply.github.com>
2022-05-05 11:41:36 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://poxyaz7yj4.execute-api.eu-west-1.amazonaws.com/default/CC-Portfolio-predict",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
