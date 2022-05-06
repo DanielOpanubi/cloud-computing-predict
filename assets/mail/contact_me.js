eb8a4f82a62f8e433d6a757449adab6222e9bcf1
DanielOpanubi <99402393+DanielOpanubi@users.noreply.github.com>
2022-05-06 12:38:00 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,8 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://la9ymcr1z2.execute-api.eu-west-1.amazonaws.com/default/CC-Portfolio-predict",
-                // ------------------------------------------------------  
+                url: "https://0a4stw6zbj.execute-api.eu-west-1.amazonaws.com/default/CC-Portfolio-predict",
                 type: "POST",
                 data: JSON.stringify(json),
                 cache: false,
